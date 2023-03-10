22d351ebee39ee485bcf0e0108f1a8af7b6315bf
HuoShanX <127390750+HuoShanX@users.noreply.github.com>
2023-03-10 16:26:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxf0d830d12b7ddea4",
+  appsecret: "29857f7125ba5b04c8f377011281654f",
+  template_id: "gIRUW1d-FLNsrDtX4f4VfGItwrrJ7mBl1-iNwfPL4Rs",
+  touser: "oonz25gaI9AfRZLexxNrtSjwWvPg",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "济南",
+  first_date: "2018-9-20",
 };
